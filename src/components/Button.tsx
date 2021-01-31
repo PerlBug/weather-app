7b65f413d91d342c46fa855bb954af09687de7ea
Mko G <30924553+PerlBug@users.noreply.github.com>
2021-01-31 22:33:51 +1100

    feat(Button): created a button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+
+interface ButtonProps {
+  children?: any;
+  onClick?: () => void;
+  className?: string;
+  loading?: boolean;
+}
+
+function Button(props: ButtonProps) {
+  const { children, onClick, className, loading } = props;
+  return (
+    <button
+      className={`p-2 bg-red-500 text-center ${className}`}
+      onClick={onClick}
+    >
+      <div className="flex">
+        {" "}
+        {loading && <>loading...</>}
+        {!loading && <>{children}</>}
+      </div>
+    </button>
+  );
+}
+
+export default Button;
