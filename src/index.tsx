95cc8dca73454cb31c6ec6d7785c7cddacf52dad
Mko G <30924553+PerlBug@users.noreply.github.com>
2021-01-31 22:45:32 +1100

    feat(index): added react toast notifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ToastProvider } from "react-toast-notifications";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ToastProvider>
+    <App />
+  </ToastProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
