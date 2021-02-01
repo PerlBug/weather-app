7fbfd74890977ad86e22b3f38e8fe63af1fc220c
Mko Galoyan <galoyanmko@gmail.com>
2021-02-01 21:13:00 +1100

    feat(App.test.tsx): added basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { mount, shallow } from 'enzyme';
+import { ToastProvider } from 'react-toast-notifications';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<ToastProvider><App /></ToastProvider>);
 });
+
+//TODO - more test cases
