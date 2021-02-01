a4d2a72ff58803c01750c71270f0bbefb00922ee
Mko Galoyan <galoyanmko@gmail.com>
2021-02-01 21:14:14 +1100

    chore(App.test.tsx): remove unusued import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import { ToastProvider } from 'react-toast-notifications';
 
 it("renders without crashing", () => {
